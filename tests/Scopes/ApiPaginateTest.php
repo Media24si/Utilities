109c641da0403854f69a886d45b8c10381443ff9
Adam Stradovnik <adam.stradovnik@gmail.com>
2017-10-26 12:52:46 +0200

    api paginate scope test

--- tests/Scopes/ApiPaginateTest.php
+++ tests/Scopes/ApiPaginateTest.php
@@ -10,6 +10,11 @@ use Media24si\Utilities\Pagination\ApiPaginator;
 class ApiPaginate extends \Illuminate\Database\Eloquent\Model
 {
     use \Media24si\Utilities\Scopes\ApiPaginate;
+
+    public function hydrate()
+    {
+        return collect(['item3', 'item4']);
+    }
 }
 
 class ApiPaginateTest extends TestCase
@@ -34,34 +39,43 @@ class ApiPaginateTest extends TestCase
         $builder = $this->getBuilder();
         $builder->setModel($this->model);
 
+        \Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver = m::mock('Illuminate\Database\ConnectionResolverInterface'));
+        $resolver->shouldReceive('connection')->andReturn($mockConnection = m::mock('stdClass'));
+
         $builder->getQuery()->shouldReceive('getCountForPagination')->once()->andReturn(2);
         $builder->getQuery()->shouldReceive('forPage')->once()->with(1, 2)->andReturnSelf();
         $builder->getQuery()->shouldReceive('get')->once()->with(['*'])->andReturn(collect(['item3', 'item4']));
-        $builder->getQuery()->shouldReceive('paginate')->once()->andReturn($this->p);
 
         $result = $builder->apiPaginate($perPage);
 
-        $this->assertEquals($builder, $result);
+        $pageInfo = [
+            'data' => ['item3', 'item4'],
+            'pagination' => [
+                'total' => 2,
+                'per_page' => 2,
+                'current_page' => 1,
+                'last_page' => 1,
+                'next_page_url' => null,
+                'prev_page_url' => null,
+                'from' => 1,
+                'to' => 2
+            ]
+        ];
+        $this->assertEquals($pageInfo, $result->toArray());
     }
 
     protected function getBuilder()
     {
         return new Builder($this->getMockQueryBuilder());
-        // $builder = m::mock('Illuminate\Database\Eloquent\Builder', [$this->getMockQueryBuilder()]);
-        // $builder->shouldReceive('setModel')->andReturn($this->model);
-        // return $builder;
     }
 
     protected function getMockQueryBuilder()
     {
-        // $query = m::mock(\Illuminate\Database\Query\Builder::class, [
-        //     m::mock('Illuminate\Database\ConnectionInterface'),
-        //     new \Illuminate\Database\Query\Grammars\Grammar,
-        //     m::mock('Illuminate\Database\Query\Processors\Processor'),
-        // ]);
-        // $query->shouldReceive('from');
-        $query = m::mock(\Illuminate\Database\Query\Builder::class);
-        $query->shouldReceive('from');
+        $query = m::mock(\Illuminate\Database\Query\Builder::class, [
+            m::mock('Illuminate\Database\ConnectionInterface'),
+            new \Illuminate\Database\Query\Grammars\Grammar,
+            m::mock('Illuminate\Database\Query\Processors\Processor'),
+        ])->makePartial();
 
         return $query;
     }
